76c12af0d8bd2c3916fdaddaf98206fc5f6d2a1d
ckidwell <erebusman@gmail.com>
2024-01-15 10:46:16 -0800

    add MonoGame.Extended.Tweening NuGet package

--- DMGSimpleUI/DMG/Samples/SampleSceneNavigator.cs
+++ DMGSimpleUI/DMG/Samples/SampleSceneNavigator.cs
@@ -1,6 +1,25 @@
+using DMGSimpleUI.DMG.Models;
+
 namespace DMGSimpleUI.DMG.Samples;
 
 public class SampleSceneNavigator
 {
+    public DMGTransition transition;
+    
+    public void Update()
+    {
+        // foreach (var item in _elements)
+        // {
+        //     item.Update();
+        // }
+    }
+
+    public void Draw()
+    {
+        // foreach (var item in _elements)
+        // {
+        //     item.Draw();
+        // }
+    } 
     
 }
